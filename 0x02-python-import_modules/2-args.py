f0e4d9728b27b188a1b0431e80601faeda792dfc
horlaidey <salaudeenolaboy@gmail.com>
2023-08-10 13:20:38 -0700

    Bash corrections

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-from sys import argv
-if len(argv) == 2:
-    print("{:d} argument".format(len(argv)-1))
-    print("{:d}: {:s}".format(len(argv) - 1, argv[1]))
-else:
-    print("{:d} arguments".format(len(argv) - 1))
-    for i in range(1, len(argv)):
-        print("{:d}: {:s}".format(i, argv[i]))
+if __name__ == "__main__":
+    from sys import argv
+    if len(argv) == 2:
+        print("{:d} argument".format(len(argv)-1))
+        print("{:d}: {:s}".format(len(argv) - 1, argv[1]))
+    else:
+        print("{:d} arguments".format(len(argv) - 1))
+        for i in range(1, len(argv)):
+            print("{:d}: {:s}".format(i, argv[i]))

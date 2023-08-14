609ac5e84b2fe1f2fce37b02dd1ffdacd8e4fe9a
horlaidey <salaudeenolaboy@gmail.com>
2023-08-14 16:25:02 -0700

    Late night approach

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix != [[]]:
-        for row in matrix:
-            for i in range(len(row) - 1):
-                print("{:d}".format(row[i]), end=" ")
-            print("{:d}".format(row[i + 1]))
-    else:
-        print("")
+    for row in matrix:
+        for i in row:
+            print("{:d}".format(i), end=" " if i != row[-1] else "")
+        print()

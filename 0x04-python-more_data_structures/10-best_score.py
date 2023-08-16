bc482519c5b2b51b706cf48ad0b07a422ed827d3
horlaidey <salaudeenolaboy@gmail.com>
2023-08-16 16:58:05 -0700

    List comprehension

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary != None:
+        keys = list(a_dictionary.keys())
+        tmp_best = 0
+        best_key = ""
+        for i in keys:
+            if a_dictionary[i] > tmp_best:
+                tmp_best = a_dictionary[i]
+                best_key = i
+        return best_key
+    else:
+        return None

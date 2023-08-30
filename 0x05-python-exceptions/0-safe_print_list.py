c7c21cddba3f0080b8f88c85118892621af2abcb
Horlaidey <salaudeenolaboy@gmail.com>
2023-08-30 05:08:04 -0700

    magixcalc

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -10,12 +10,12 @@ def safe_print_list(my_list=[], x=0):
     Returns:
         The number of elements printed.
     """
-    elem_print = 0
+    ret = 0
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
-            elem_print += 1
+            ret += 1
         except IndexError:
             break
     print("")
-    return(elem_print)
+    return (ret)

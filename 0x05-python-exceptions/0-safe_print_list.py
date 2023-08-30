c15bbfbaec7198e4df28556b65bed30df405b864
Horlaidey <salaudeenolaboy@gmail.com>
2023-08-30 11:14:46 +0000

    safe print commit

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    elem_print = 0
+    for i in range(x):
+        print("{}".format(my_list[i]), end="")
+        elem_print += 1
+        except IndexError:
+            break
+    print("")
+    return(elem_print)

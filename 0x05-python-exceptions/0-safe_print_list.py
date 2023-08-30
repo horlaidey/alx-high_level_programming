7b51ca45a6ab4034a4943e0b5faa3b980d8e7b43
Horlaidey <salaudeenolaboy@gmail.com>
2023-08-30 04:45:48 -0700

    documented

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,13 +3,14 @@ def safe_print_list(my_list=[], x=0):
     """Prints x-amount of element in a list
 
     Args:
-    my_list - the list from which element
-    is printed
-    x - the number of element expected to
-    be printed
+        my_list (list): the list from which element
+        is printed
+        x (int): the number of element expected to
+        be printed
 
     Return:
-    the amount of printed element"""
+        the amount of printed element
+        """
     elem_print = 0
     for i in range(x):
         try:
